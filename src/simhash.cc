c6c91e43815ff79f8fb0e66cd19ab653966c2164
y_benjo <oreno_bukiwo_sitteruka@yahoo.co.jp>
2010-12-14 17:36:03 +0900

    類似度を定義に戻す

--- src/simhash.cc
+++ src/simhash.cc
@@ -245,8 +245,7 @@ void SimHash::search_b_nearest_data(unint b){
   }
 }
 
-//正確なコサイン類似度ではなく、
-//d_feature に含まれる要素のみでコサイン類似度を計算している
+//正確なコサイン類似度
 double SimHash::calculate_cosine_distance(unint q_id, unint d_id){
   double norm_q = 0.0, norm_d = 0.0, inner = 0.0;
   
@@ -255,13 +254,12 @@ double SimHash::calculate_cosine_distance(unint q_id, unint d_id){
     norm_d += pow((*i).second, 2.0);
     if(query_feature.end() != query_feature.find((*i).first)){
       inner += (*i).second * query_feature[(*i).first];
-      norm_q += pow(query_feature[(*i).first], 2.0);
     }
   }
 
-//   for(unordered_map<unint, double>::iterator i = query_feature.begin(); i != query_feature.end(); ++i){
-//     norm_q += pow(i->second, 2.0);
-//   }
+  for(unordered_map<unint, double>::iterator i = query_feature.begin(); i != query_feature.end(); ++i){
+    norm_q += pow(i->second, 2.0);
+  }
 
   if(norm_q > 0 && norm_d > 0){
     return inner / (pow(norm_q, 0.5) * pow(norm_d, 0.5));
@@ -693,6 +691,7 @@ void SimHash::bit_xor(int k){
 
 void SimHash::query_normalization(bool normal_flag){
   if(normal_flag){
+    cout << "query normalization" << endl;
     double sum = 0.0;
     for(unordered_map<unint, double>::iterator i = query_feature.begin(); i != query_feature.end(); ++i){
       sum += i->second;
